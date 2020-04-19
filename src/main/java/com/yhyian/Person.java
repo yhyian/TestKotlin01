c3628e9df3df3f37bbf6bb445ad4d012beaca5ce
Yi-Hwa Yian <yhyian@gmail.com>
2020-04-19 12:58:07 +0800

    Initial commit

---
+++ src/main/java/com/yhyian/Person.java
@@ -0,0 +1,7 @@
+package com.yhyian;
+
+public class Person {
+    public void hello(){
+        System.out.println("This Hello from Person Class");
+    }
+}
---
+++ src/main/kotlin/com/yhyian/HelloWorld.kt
@@ -0,0 +1,14 @@
+package com.yhyian
+
+fun main(args: Array<String>) {
+    //print("Hello, World. This is Kotlin!")
+    val p = Human();
+    p.hello();
+}
+
+
+class Human(){
+    fun hello(){
+        print("Kotlin Hello")
+    }
+}
