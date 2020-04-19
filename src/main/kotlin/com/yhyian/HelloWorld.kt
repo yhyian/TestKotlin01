8dd5ea852a91d912c33a209dbf9fd71f45ff9e66
Yi-Hwa Yian <yhyian@gmail.com>
2020-04-19 13:24:53 +0800

    Update Kotlin Test Message

--- src/main/kotlin/com/yhyian/HelloWorld.kt
+++ src/main/kotlin/com/yhyian/HelloWorld.kt
@@ -4,6 +4,12 @@ fun main(args: Array<String>) {
     //print("Hello, World. This is Kotlin!")
     val p = Human();
     p.hello();
+    val age : Int = 19;
+    var grade : Float = 99.5f;
+    var name : String ;
+    name = "yhyian";
+
+
 }
 
 
